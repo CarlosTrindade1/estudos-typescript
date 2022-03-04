5ee6121a7c560145df811d1f6863cd108d3d020e
Carlos Trindade <carlos.trindade@estudante.ifms.edu.br>
2022-03-04 14:47:20 -0400

    Associação em classes - TypeScript

--- src/Classes e Interfaces - POO/A029-associacao/main.ts
+++ src/Classes e Interfaces - POO/A029-associacao/main.ts
@@ -1,10 +1,13 @@
 import { Caneta, Escritor, MaquinaEscrever } from '.';
 
-const escritor = new Escritor('Carlos Trindade');
+const escritor1 = new Escritor('Carlos Trindade');
+const escritor2 = new Escritor('Eduardo da Silva');
 
 const caneta = new Caneta('Caneta Bic');
 const maquinaEscrever = new MaquinaEscrever('Máquina Bic');
 
-// escritor.ferramenta = caneta;
+escritor1.ferramenta = caneta;
+escritor2.ferramenta = maquinaEscrever;
 
-escritor.escrever();
+escritor1.escrever();
+escritor2.escrever();
