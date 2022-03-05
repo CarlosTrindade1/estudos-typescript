142e91412fa3f98545727c80681c467d23e8120e
Carlos Trindade <carlos.trindade@estudante.ifms.edu.br>
2022-03-04 20:58:44 -0400

    Interfaces em TypeScript

---
+++ src/Classes e Interfaces - POO/A033-interfaces/index.ts
@@ -0,0 +1,17 @@
+interface TipoPessoa {
+  nome: string;
+  sobrenome: string;
+  nomeCompleto(): string;
+}
+
+export class Pessoa implements TipoPessoa {
+  constructor(readonly nome: string, readonly sobrenome: string) {}
+
+  public nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Carlos', 'Trindade');
+
+console.log(pessoa.nomeCompleto());
