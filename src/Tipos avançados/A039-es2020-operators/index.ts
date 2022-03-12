3a1d4b81c14a89fd7589663f290bdc1914e37196
Carlos Trindade <carlos.trindade@estudante.ifms.edu.br>
2022-03-12 10:25:40 -0400

    Operador de coalescência nula em JS e TS

---
+++ src/Tipos avançados/A039-es2020-operators/index.ts
@@ -0,0 +1,14 @@
+// Encadeamento opcional e operador de coalescência nula
+type Documento = {
+  titulo: string;
+  texto: string;
+  data?: Date;
+};
+
+const documento: Documento = {
+  titulo: 'O título',
+  texto: 'O texto',
+  // data: new Date(),
+};
+
+console.log(documento.data?.toDateString() ?? 'Ixi, não existe data.');
